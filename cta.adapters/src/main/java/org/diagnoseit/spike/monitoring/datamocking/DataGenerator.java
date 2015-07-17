e36b81865331622a42052f4527ee3f0e55a0cba9
alexander.wert <alexander.wert@novatec-gmbh.de>
2015-07-17 09:50:36 +0200

    extended template for data mocking

--- cta.adapters/src/main/java/org/diagnoseit/spike/monitoring/datamocking/DataGenerator.java
+++ cta.adapters/src/main/java/org/diagnoseit/spike/monitoring/datamocking/DataGenerator.java
@@ -1,7 +1,11 @@
 package org.diagnoseit.spike.monitoring.datamocking;
 
+import java.util.Random;
+
 public class DataGenerator implements Runnable {
-	
+	private static final long AVG_INTER_DATA_TIME = 50; // in ms
+	private static final double VARIANCE = 0.5; // in % from avg
+	private static final Random rand = new Random(System.currentTimeMillis());
 	private static DataGenerator instance;
 
 	public static DataGenerator getInstance() {
@@ -28,8 +32,23 @@ public class DataGenerator implements Runnable {
 
 	public void run() {
 		while (run) {
+			double pCent = rand.nextDouble() * 2.0 * VARIANCE - VARIANCE;
+
+			long sleepTime = AVG_INTER_DATA_TIME
+					+ (long) (pCent * (double) AVG_INTER_DATA_TIME);
+			try {
+				Thread.sleep(sleepTime);
+			} catch (InterruptedException e) {
+				// ignore exception
+				e.printStackTrace();
+			}
+			createNewMonitoringRecord();
 
 		}
+	}
+
+	private void createNewMonitoringRecord() {
+
 	}
 
 }
