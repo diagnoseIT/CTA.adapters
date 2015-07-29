982ea428dca66416430f4de1279f1e9e04bfb71b
hegerchr <christoph.heger@novatec-gmbh.de>
2015-07-29 09:26:52 +0200

    Dummy rule with baseline

---
+++ cta.adapters/src/main/java/org/diagnoseit/spike/result/ProblemInstance.java
@@ -0,0 +1,14 @@
+package org.diagnoseit.spike.result;
+
+public class ProblemInstance {
+	
+	private String signature;
+
+	public String getSignature() {
+		return signature;
+	}
+
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+}
